16520c25f8a212532f6fa4d27549029b45e08360
dpdesai02 <dpdesai2884@gmail.com>
2023-04-25 17:30:45 +0100

    com-nopcommerce-sw-3-dhara

---
+++ src/homepage/TopMenuTest.java
@@ -0,0 +1,112 @@
+package homepage;
+
+import Utilities.Utility;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class TopMenuTest extends Utility {
+
+    @Before
+    public void setUp() {
+        openBrowser(baseUrl);
+    }
+
+    @After
+    public void tearDown() {
+        closeBrowser();
+    }
+
+    @Test
+    public void verifyPageNavigationToMenu() {
+
+    }
+
+    /*
+    1.1 create method with name "selectMenu" it has one parameter name "menu" of type
+    string
+     */
+    public static void selectMenu(String menu) {
+        //create a list to store all menu options
+        List<WebElement> menuList = driver.findElements(By.xpath("//div[@class = 'header-menu']/ul/li"));
+        try {
+            //loop through list with for loop
+            for (WebElement menuOption : menuList) {
+                System.out.println(menuOption.getText());
+                //if option matches with the parameter then click
+                if (menuOption.getText().equals(menu)) {
+                    menuOption.click();
+                }
+            }
+            //catch exceptions
+        } catch (StaleElementReferenceException e) {
+            menuList = driver.findElements(By.xpath("//div[@class = 'header-menu']/ul/li"));
+        }
+    }
+
+    /*
+    This method should click on the menu whatever name is passed as parameter.
+     */
+    @Test
+    public void verifyPageNavigation() throws InterruptedException {
+        //call selectMenu method with parameter books
+        selectMenu("Books");
+        //define expected
+        String expectedPage = "Books";
+        //get actual
+        String actualPage = getTextFromElement(By.xpath("//h1[contains(text(),'Books')]"));
+        //verify expected and actual are same
+        Assert.assertEquals("page relocation fail", expectedPage, actualPage);
+    }
+
+    /*
+    create the @Test method name verifyPageNavigation.use selectMenu method to
+    select the Menu and click on it and verify the page navigation.
+     */
+    @Test
+    public void userShouldNavigateToComputerPageSuccessfully() {
+        clickOnElement(By.linkText("Computers"));
+
+    }
+    @Test
+    public void userShouldNavigateToElectronicsPageSuccessfully() {
+        clickOnElement(By.linkText("Electronics"));
+
+    }
+    @Test
+    public void userShouldNavigateToApparelPageSuccessfully() {
+        clickOnElement(By.linkText("Apparel"));
+
+    }
+
+    @Test
+    public void userShouldNavigateToDigitalDownloadsPageSuccessfull() {
+        clickOnElement(By.linkText("Digital downloads"));}
+
+
+    @Test
+    public void userShouldNavigateToBooksPageSuccessfully() {
+        clickOnElement(By.linkText("Books"));}
+
+
+    @Test
+    public void userShouldNavigateToJewelryPageSuccessfully() {
+        clickOnElement(By.linkText("Jewelry"));
+
+    }
+
+    @Test
+    public void userShouldNavigateToGiftCardsPageSuccessfully() {
+        clickOnElement(By.linkText("Gift Cards"));
+
+    }
+
+
+}
